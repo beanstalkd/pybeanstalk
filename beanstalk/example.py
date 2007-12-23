0613f5b3f12d45dd38f4cf305b40eebb92310be3
Erich Heine <sophacles@gmail.com>
2007-12-23 21:22:43 +0000

    Adding a trivial producer and consumer client program for example purposes

---
+++ beanstalk/example.py
@@ -0,0 +1,53 @@
+# stdlib imports
+import sys
+import time
+import select
+
+# pybeanstalk imports
+import serverconn
+import job
+
+def producer_main(connection):
+    i = 0
+    while True:
+        data = 'This is data to be consumed (%s)!' % (i,)
+        print data
+        data = job.Job(data=data, conn=connection)
+        data.put()
+        time.sleep(1)
+        i += 1;
+
+def consumer_main(connection):
+    while True:
+        j = connection.reserve()
+        print 'got job! job is: %s' % j.data
+        j.delete()
+
+def main():
+    try:
+        print 'handling args'
+        clienttype = sys.argv[1]
+        server = sys.argv[2]
+        try:
+            port = int(sys.argv[3])
+        except:
+            port = 11300
+
+        print 'setting up connection'
+        connection = serverconn.ServerConn(server, port)
+        connection.job = job.Job
+        if clienttype == 'producer':
+            print 'starting producer loop'
+            producer_main(connection)
+        elif clienttype == 'consumer':
+            print 'starting consumer loop'
+            consumer_main(connection)
+        else:
+            raise Exception('foo')
+    except Exception, e:
+        print "usage: example.py TYPE server [port]"
+        print " TYPE is one of: [producer|consumer]"
+        raise
+        sys.exit(1)
+if __name__ == '__main__':
+    main()
