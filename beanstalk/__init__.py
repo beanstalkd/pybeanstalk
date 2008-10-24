1c5ddb32e16e7cc0f14f821e70b7afb7af1bc3ca
Dustin Sallings <dustin@spy.net>
2008-10-24 09:28:44 -0700

    Expose the twisted client at the top level of the package.

--- beanstalk/__init__.py
+++ beanstalk/__init__.py
@@ -2,4 +2,5 @@ import serverconn
 import job
 import errors
 import protohandler
-__all__ = [protohandler, serverconn, errors, job]
+import twisted_client
+__all__ = [protohandler, serverconn, errors, job, twisted_client]
