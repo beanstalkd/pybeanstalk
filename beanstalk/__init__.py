20575adac3cea711fc99ef02fccd20a35c62161c
Erich Heine <eheine@pengana.iti.uiuc.edu>
2009-02-09 09:28:37 -0600

    Made the twisted_client a conditional import.
    This was in response to a comment on issue #5, users without twisted get
    an error.

--- beanstalk/__init__.py
+++ beanstalk/__init__.py
@@ -2,5 +2,9 @@ import serverconn
 import job
 import errors
 import protohandler
-import twisted_client
-__all__ = [protohandler, serverconn, errors, job, twisted_client]
+__all__ = [protohandler, serverconn, errors, job]
+try:
+    import twisted_client
+    __all__.append[twisted_client]
+except ImportError:
+    pass
