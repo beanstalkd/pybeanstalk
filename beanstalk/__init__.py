a85e185295b90f431d85cc02679b207d7b108dc7
Erich Heine <eheine@pengana.iti.uiuc.edu>
2009-04-23 15:31:28 -0500

    Fixed [] instead of () typo. This was reported by many many people, none of whom I can remember or find reference to unfortunately.

--- beanstalk/__init__.py
+++ beanstalk/__init__.py
@@ -5,6 +5,6 @@ import protohandler
 __all__ = [protohandler, serverconn, errors, job]
 try:
     import twisted_client
-    __all__.append[twisted_client]
+    __all__.append(twisted_client)
 except ImportError:
     pass
