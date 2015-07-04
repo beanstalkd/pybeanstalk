344edb65d5b37492f82585e57d2f0e589d734219
Petri Savolainen <petri.savolainen@iki.fi>
2015-07-04 17:50:24 +0300

    __all__ should contain strings

--- beanstalk/__init__.py
+++ beanstalk/__init__.py
@@ -2,7 +2,7 @@ import serverconn
 import job
 import errors
 import protohandler
-__all__ = [protohandler, serverconn, errors, job]
+__all__ = ["protohandler", "serverconn", "errors", "job"]
 try:
     import twisted_client
     __all__.append(twisted_client)
