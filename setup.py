05305b03653422f6e3588fc49376e0ae7f95e614
Petri Savolainen <petri.savolainen@koodaamo.fi>
2015-07-07 10:16:30 +0300

    bump version, add dependencies, update docs

--- setup.py
+++ setup.py
@@ -3,17 +3,26 @@
 from setuptools import setup, find_packages
 
 description_long = '''
-A client library for beanstalkd. beanstalkd is a lightweight queuing daemon
+A pure-Python client library for beanstalkd. beanstalkd is a lightweight queuing daemon
 based on libevent. It is meant to be used across multiple systems, and takes
 inspiration from memcache. More details on beanstalk can be found at:
 http://xph.us/software/beanstalkd/
 
-This client library aims to be simple and extensible. It curently provides no
-fancy wrappers, or e.g. thread-wrangling
+This client library aims to be simple and extensible. It currently provides no
+fancy wrappers, or e.g. thread-wrangling.
+
+To install, just run python setup.py install from this directory, or use the shortcuts
+in the Makefile.
+
+Please see the examples directory for usage examples.
+
+The package home is at https://github.com/beanstalkd/pybeanstalk, with an issue tracker
+and a wiki. Bug reports and pull requests most welcome.
+
 '''
 
 setup(name='pybeanstalk',
-      version='0.11.1',
+      version='1.10.1',
       description='A python client library for beanstalkd.',
       long_description = description_long,
       author='Erich Heine',
@@ -35,6 +44,9 @@ setup(name='pybeanstalk',
         'Topic :: Software Development :: Object Brokering',
         'Topic :: System'],
       packages=['beanstalk'],
-      extras_require={'twisted': ["Twisted>=15.2.1"]}
+      install_requires=["pyaml"],
+      extras_require={'twisted': ["zope.interface", "Twisted>=15.2.1"]},
+      include_package_data=True,
+      zip_safe=False
 )
 
