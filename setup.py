d6ecf131e4c1a836cede5323df2c39002502a84a
Petri Savolainen <petri.savolainen@koodaamo.fi>
2015-07-07 10:31:47 +0300

    more descriptive docs, add keywords to package

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ based on libevent. It is meant to be used across multiple systems, and takes
 inspiration from memcache. More details on beanstalk can be found at:
 http://xph.us/software/beanstalkd/
 
-This client library aims to be simple and extensible. It currently provides no
-fancy wrappers, or e.g. thread-wrangling.
+This client library aims to be simple and extensible. It provides both a single thread,
+single connection serialized (select-based) beanstalk connection with optional, simple
+thread pool implementation, and a basic Twisted client. Both can be used directly, or
+be used as basis for for more sophisticated client applications. Please see the examples
+directory for usage examples.
 
 To install, just run python setup.py install from this directory, or use the shortcuts
 in the Makefile.
-
-Please see the examples directory for usage examples.
 
 The package home is at https://github.com/beanstalkd/pybeanstalk, with an issue tracker
 and a wiki. Bug reports and pull requests most welcome.
@@ -25,6 +26,7 @@ setup(name='pybeanstalk',
       version='1.10.1',
       description='A python client library for beanstalkd.',
       long_description = description_long,
+      keywords='beanstalkd, twisted',
       author='Erich Heine',
       author_email='sophacles@gmail.com',
       url='https://github.com/beanstalkd/pybeanstalk',
